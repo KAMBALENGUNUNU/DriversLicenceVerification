e3f0aeeadfc416a517ca4aac9fcb88126504e690
KAMBALENGUNUNU <kambalengununudaniel@gmail.com>
2024-09-03 19:09:22 +0200

    code update

--- contract.sol
+++ contract.sol
@@ -10,3 +10,15 @@ contract DriversLicenseVerification {
         uint256 expiryDate;
         bool isValid;
     }
+        // Mapping to store licenses by license number
+    mapping(string => License) private licenses;
+    
+    // Address of the contract owner
+    address public owner;
+    
+    // Event emitted when a new license is registered
+    event LicenseRegistered(string licenseNumber, string holderName);
+    
+    // Event emitted when a license is revoked
+    event LicenseRevoked(string licenseNumber);
+    
